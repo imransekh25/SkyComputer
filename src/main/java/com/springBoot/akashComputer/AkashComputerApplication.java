362d1edcb617e8b09eb5a4811eb99d53d0a18c4d
Imran Sekh <imransekh25@gmail.com>
2022-04-14 13:48:50 +0530

    Added my files

---
+++ src/main/java/com/springBoot/akashComputer/AkashComputerApplication.java
@@ -0,0 +1,13 @@
+package com.springBoot.akashComputer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkashComputerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkashComputerApplication.class, args);
+	}
+
+}
